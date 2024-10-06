178b58ed3d2565f1d7758ea0b61df8ca0234c3de
kumar1310aryan <kumar1310aryan@gmail.com>
2024-10-06 07:45:37 +0530

    responsiveness and hoverOnDigitize

--- Das Grune/script.js
+++ Das Grune/script.js
@@ -23,7 +23,6 @@ function mousemoveOnDigitize() {
     });
   });
 }
-// mousemoveOnDigitize();
 
 function scrambleText(elementId) {
   const element = document.getElementById(elementId);
@@ -77,3 +76,10 @@ function designHover() {
   }
 }
 designHover();
+
+const tl = gsap.timeline();
+
+tl.fromTo(".digitizeh1", 
+  { opacity: 0, y: 50 }, 
+  { opacity: 1, y: 0, duration: 1.5, stagger: 0.5, ease: "power3.out", delay: 0.3 }
+);
