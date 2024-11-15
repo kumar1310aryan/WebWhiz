cbe5dad8a72778a650a908f6403f178557abc4fe
kumar1310aryan <kumar1310aryan@gmail.com>
2024-11-15 08:11:37 +0530

    cocota animations

---
+++ Cocota-creative agency/script.js
@@ -0,0 +1,16 @@
+// const branding = document.getElementById("branding");
+// const brandingDesign = document.getElementById("branding-design");
+// const design = document.getElementById("design");
+
+// branding.addEventListener("mouseover", () => {
+//   brandingDesign.style.width = "120vw";
+// });
+
+// design.addEventListener("mouseover", () => {
+//   brandingDesign.style.width = "120vw";
+//   design.style.width = "60vw"; // Expand design section as well when hovering over branding section
+// });
+
+// branding.addEventListener("mouseout", () => {
+//   brandingDesign.style.width = ""; // Reset to default when hover ends
+// });
