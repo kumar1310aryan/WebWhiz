fb4254c530e744a5482f6e7c69f7dde4ca5d7014
kumar1310aryan <kumar1310aryan@gmail.com>
2024-11-19 08:49:19 +0530

    ochi eye animation

--- OCHI/src/components/Eyes.jsx
+++ OCHI/src/components/Eyes.jsx
@@ -1,18 +1,45 @@
+import { useEffect, useState } from "react";
+
 function Eyes() {
+  const [rotate, setRotate] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
+
+      var angle = Math.atan2(deltaX, deltaY) * (180 / Math.PI);
+      setRotate(angle);
+    });
+  });
+
   return (
     <div className="eyes w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')]">
         <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  flex justify-between align-middle gap-11">
           <div className="w-[14vw] h-[14vw] bg-white rounded-full flex items-center justify-center">
             <div className="relative w-3/5 h-3/5 rounded-full bg-zinc-900">
-              <div className="line w-full h-8 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+              <div
+                style={{
+                  transform: `translate(-50%,-50%) rotate(${rotate - 180}deg)`,
+                }}
+                className="line w-full h-8 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]"
+              >
                 <div className="w-8 h-8 rounded-full bg-zinc-100"></div>
               </div>
             </div>
           </div>
           <div className="w-[14vw] h-[14vw] bg-white rounded-full flex items-center justify-center">
             <div className="relative w-3/5 h-3/5 rounded-full bg-zinc-900">
-              <div className="absolute line w-full h-8 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+              <div
+                style={{
+                  transform: `translate(-50%,-50%) rotate(${rotate - 180}deg)`,
+                }}
+                className="absolute line w-full h-8 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]"
+              >
                 <div className="w-8 h-8 rounded-full bg-zinc-100"></div>
               </div>
             </div>
