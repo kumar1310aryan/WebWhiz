4610744b228964f4db09d94e025fbe7d3a54745c
kumar1310aryan <kumar1310aryan@gmail.com>
2024-11-21 08:46:21 +0530

    ochi footer

--- OCHI/src/App.jsx
+++ OCHI/src/App.jsx
@@ -5,7 +5,12 @@ import About from "./components/About";
 import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="w-full min-h-screen text-white bg-zinc-900">
       <NavBar />
@@ -15,6 +20,7 @@ function App() {
       <Eyes />
       <Featured />
       <Cards />
+      <Footer />
     </div>
   );
 }
---
+++ OCHI/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 flex">
+      <div className="left w-[50%] h-full flex flex-col justify-between p-[3vw]">
+        <h1 className="text-[5vw] font-bold tracking-tighter transform scale-y-150 leading-none">
+          EYE- <br /> OPENING
+        </h1>
+        <h1 className="text-[2vw] tracking-tight">Ochi</h1>
+      </div>
+      <div className="left w-[50%] h-full flex flex-col justify-between p-[3vw]">
+        <h1 className="text-[5vw] font-bold tracking-tighter transform scale-y-150 leading-none">
+          PRESENTATIONS
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
